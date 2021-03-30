3b8d6930ee034a15a1a3de7c105d6f6c4202b208
Justin Hager <developerjustinhager@pm.me>
2021-03-30 10:37:17 -0700

    initialize audio capture with MediaRecorder API

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -13,7 +13,12 @@
       </ul>
 
       <input v-model="newMessageText" class="input" />
-
+      <br />
+      <h5>Record Audio</h5>
+      <button v-if="!recorder" @click="record()">Record</button>
+      <button v-else @click="stop()">Stop</button>
+      <audio v-if="newAudio" :src="newAudioURL" controls></audio>
+      <br />
       <button
         :disabled="!newMessageText || loading"
         class="button is-success"
@@ -41,6 +46,8 @@ export default {
       newMessageText: '',
       loading: false,
       messages: [],
+      newAudio: null,
+      recorder: null,
     };
   },
   computed: {
@@ -49,6 +56,9 @@ export default {
     },
     messagesCollection() {
       return db.doc(`chats/${this.chatId}`).collection('messages');
+    },
+    newAudioURL() {
+      return URL.createObjectURL(this.newAudio);
     },
   },
   firestore() {
@@ -69,6 +79,35 @@ export default {
       });
       this.loading = false;
       this.newMessageText = '';
+    },
+    async record() {
+      this.newAudio = null;
+
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+
+      const options = { mimeType: 'audio/webm' };
+      const recordedChuncks = [];
+      this.recorder = new MediaRecorder(stream, options);
+
+      this.recorder.addEventListener('dataavailable', e => {
+        if (e.data.size > 0) {
+          recordedChuncks.push(e.data);
+        }
+      });
+
+      this.recorder.addEventListener('stop', () => {
+        this.newAudio = new Blob(recordedChuncks);
+        console.log(this.newAudio);
+      });
+
+      this.recorder.start();
+    },
+    async stop() {
+      this.recorder.stop();
+      this.recorder = null;
     },
   },
 };
